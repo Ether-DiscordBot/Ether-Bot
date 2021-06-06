b27790f4c0e1106409ecca8ff2a6bc045bb570f3
holy-tanuki <real.kar0t@gmail.com>
2021-06-07 00:50:16 +0200

    Add help command

--- commands/misc/misc.py
+++ commands/misc/misc.py
@@ -8,26 +8,40 @@ class Misc(commands.Cog):
         self.client = client
 
     @commands.command()
+    async def help(self, ctx):
+        await ctx.send("Help command")
+
+    @commands.command()
     @commands.cooldown(1, 3, commands.BucketType.user)
     async def ping(self, ctx):
         bot_latency = round(self.client.latency * 1000)
-        embed = Embed(description=':ping_pong: Pong !')
+        embed = Embed(description=":ping_pong: Pong !")
         msg = await ctx.channel.send(embed=embed)
-        embed.add_field(name='Bot latency', value='{0} ms'.format(bot_latency), inline=True)
-        embed.add_field(name='User latency', value='{0} ms'.format(
-            round((msg.created_at.timestamp() - ctx.message.created_at.timestamp()) * 1000)), inline=True)
+        embed.add_field(
+            name="Bot latency", value="{0} ms".format(bot_latency), inline=True
+        )
+        embed.add_field(
+            name="User latency",
+            value="{0} ms".format(
+                round(
+                    (msg.created_at.timestamp() - ctx.message.created_at.timestamp())
+                    * 1000
+                )
+            ),
+            inline=True,
+        )
         return await msg.edit(embed=embed)
 
     @commands.command()
     @commands.cooldown(1, 3, commands.BucketType.user)
     async def avatar(self, ctx):
         user = ctx.message.mentions[0] if ctx.message.mentions else ctx.message.author
-        embed = Embed(description="**{0.display_name}'s** [avatar]({0.avatar_url}):".format(user))\
-            .set_image(url=user.avatar_url_as(format='png', size=256))
+        embed = Embed(
+            description="**{0.display_name}'s** [avatar]({0.avatar_url}):".format(user)
+        ).set_image(url=user.avatar_url_as(format="png", size=256))
         return await ctx.channel.send(embed=embed)
 
-    @commands.command(aliases=['flip'])
+    @commands.command(aliases=["flip"])
     async def flipcoin(self, ctx):
         result = "Heads" if round(random()) else "Tails"
         return await ctx.channel.send(result)
-
