7e144cb439b0ef643b3503d0222602b12dc12978
holy-tanuki <real.kar0t@gmail.com>
2021-09-19 20:44:55 +0200

    Fix

--- ether/commands/levels/levels.py
+++ ether/commands/levels/levels.py
@@ -1,6 +1,6 @@
 from discord.ext import commands
 
-class Levels(commands.Cog, name="music"):
+class Levels(commands.Cog, name="levels"):
     def __init__(self, client):
         self.client = client
-        self.fancy_name = "Music"
+        self.fancy_name = "Levels"
