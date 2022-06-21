0ea726f1a5cad92d3aa062abf6572d026b30ed66
Holy Crusader <holy.tanukis@gmail.com>
2022-06-21 20:25:50 +0200

    vault-boy command

--- ether/cogs/image/image.py
+++ ether/cogs/image/image.py
@@ -1,13 +1,13 @@
 import io
 import os
 import re
+from typing import Union
 
 from discord import File, SlashCommandGroup
 from discord.ext import commands
 from PIL import Image as Img, ImageDraw, ImageFont
 
 ASSETS_FOLDER_PATH = "ether/cogs/image/assets/"
-REGEX_LINES = r".{0,30}(\s+|$)"
 
 
 class Image(commands.Cog, name="image"):
@@ -20,20 +20,28 @@ class Image(commands.Cog, name="image"):
     @image.command(name="hold_up")
     async def hold_up(self, ctx, text:str):
         image = ImageModifier("hold-up.png")
-        image.write(text, (10, 10), 4)
+        image.write(text, (10, 10), 30, 4)
         
         await ctx.respond(file=File(fp=image.bytes, filename=f"hold-up.png"))
+    
+    @image.command(name="vault_boy")
+    async def vault_boy(self, ctx, up:str, bottom:str):
+        image = ImageModifier("vault-boy.jpg")
+        image.write(up, (200, 33), 25, 2, "mm")
+        image.write(bottom, (200, 333), 25, 2, "mm")
         
+        await ctx.respond(file=File(fp=image.bytes, filename=f"vault_boy.jpg"))
+
 
 class ImageModifier:
     FONT = ImageFont.truetype(os.path.abspath("ether/assets/fonts/Inter-Medium.ttf"), 30)
     def __init__(self, template_path:str) -> None:
         self.template = Img.open(os.path.join(ASSETS_FOLDER_PATH, template_path))
     
-    def write(self, text: str, xy: tuple[int, int], max_lines=-1):
+    def write(self, text: str, xy: tuple[int, int], max_words:int=30, max_lines:int=-1, anchor="la"):
         draw = ImageDraw.Draw(self.template)
         
-        sentences = re.finditer(REGEX_LINES, text)
+        sentences = re.finditer(".{0," + str(max_words) + "}(\s+|$)", text)
 
         height = draw.textsize("A", ImageModifier.FONT)[1] + 5
         i=0
@@ -41,7 +49,7 @@ class ImageModifier:
             splitted = match.group().split("\\n")
             for subs in splitted:
                 if max_lines > 0 and i >= max_lines: break
-                draw.text((xy[0], xy[1] + height*i), subs.strip(), fill=(0, 0, 0), font=ImageModifier.FONT)
+                draw.text((xy[0], xy[1] + height*i), subs.strip(), fill=(0, 0, 0), font=ImageModifier.FONT, anchor=anchor)
                 i+=1
     
     @property
@@ -49,3 +57,4 @@ class ImageModifier:
         with io.BytesIO() as buffer:
             self.template.save(buffer, format="PNG", quality=75)
             return io.BytesIO(buffer.getvalue())
+        
