fb9c34bfba4423fa3bd2148bf44f130d921a3b42
Holy Crusader <holy.tanukis@gmail.com>
2022-06-21 02:00:00 +0200

    Update admin.py

--- ether/cogs/admin/admin.py
+++ ether/cogs/admin/admin.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from discord import Embed, Member, User, slash_command
+from discord import Embed, Member, SlashCommandGroup, User, slash_command
 import discord
 from discord.ext import commands
 from humanize import precisedelta
@@ -14,8 +14,10 @@ class Admin(commands.Cog, name="admin"):
     def __init__(self, client):
         self.fancy_name = "Admin"
         self.client = client
+    
+    admin = SlashCommandGroup("admin", "Admin commands!")
 
-    @slash_command()
+    @admin.command(name="ban")
     @commands.has_permissions(ban_members=True)
     async def ban(self, ctx, member: User, *, reason: str = None):
         guild = await Database.Guild.get_or_none(ctx.guild_id)
@@ -41,7 +43,7 @@ class Admin(commands.Cog, name="admin"):
                 embed=EtherEmbeds.error("Can't do that. Probably because I don't have the permissions for.")
             )
 
-    @slash_command()
+    @admin.command(name="kick")
     @commands.has_permissions(kick_members=True)
     async def kick(self, ctx, member: Member, *, reason=None):
         guild = await Database.Guild.get_or_none(ctx.guild_id)
@@ -67,7 +69,7 @@ class Admin(commands.Cog, name="admin"):
                 embed=EtherEmbeds.error("Can't do that. Probably because I don't have the permissions for.")
             )
 
-    @slash_command(name="clear")
+    @admin.command(name="clear")
     @commands.has_permissions(manage_messages=True)
     async def clear(self, ctx, amount: int):
         deleted = await ctx.channel.purge(limit=amount + 1)
@@ -75,7 +77,7 @@ class Admin(commands.Cog, name="admin"):
         embed.colour = Colors.SUCCESS
         await ctx.respond(embed=embed, delete_after=5)
 
-    @slash_command(name="slowmode")
+    @admin.command(name="slowmode")
     @commands.has_permissions(manage_channels=True)
     async def slowmode(self, ctx, cooldown: int):
         await ctx.channel.edit(slowmode_delay=cooldown)
@@ -84,7 +86,7 @@ class Admin(commands.Cog, name="admin"):
             return
         await ctx.respond(f"✅ Slowmode set to `{precisedelta(cooldown)}`!")
 
-    @slash_command(name="logs")
+    @admin.command(name="logs")
     @commands.has_permissions(manage_channels=True)
     async def logs(self, ctx, active: bool, channel: Optional[discord.TextChannel] = None):
         
